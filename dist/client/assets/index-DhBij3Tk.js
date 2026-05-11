720083682482c91c5eb53c7df14771adf36bda9f
245123737313-tech <245123737313@mvsrec.edu.in>
2026-05-11 16:24:22 +0530

    fix

---
+++ dist/client/assets/index-DhBij3Tk.js
@@ -0,0 +1,1 @@
+import{j as n,C as o}from"./index-C6xgNOdL.js";function t(){return n.jsx(o,{})}export{t as component};
---
+++ dist/server/assets/index-BFys4Muk.js
@@ -0,0 +1,15 @@
+import { jsx } from "react/jsx-runtime";
+import { C as CodingTrackPage } from "./router-BZs-Dt4A.js";
+import "@tanstack/react-query";
+import "@tanstack/react-router";
+import "react";
+import "lucide-react";
+import "@radix-ui/react-select";
+import "clsx";
+import "tailwind-merge";
+function Index() {
+  return /* @__PURE__ */ jsx(CodingTrackPage, {});
+}
+export {
+  Index as component
+};

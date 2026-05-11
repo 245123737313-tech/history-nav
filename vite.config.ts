d2054cb522336aeff74b34978ad35656924bbdaa
245123737313-tech <245123737313@mvsrec.edu.in>
2026-05-11 14:15:58 +0530

    tried to fix errors

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,22 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
+  vite: {
+    build: {
+      chunkSizeWarningLimit: 1200,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (!id.includes("node_modules")) return;
+
+            const pathParts = id.toString().split("node_modules/")[1].split("/");
+            if (pathParts[0].startsWith("@")) {
+              return `${pathParts[0]}/${pathParts[1]}`;
+            }
+            return pathParts[0];
+          },
+        },
+      },
+    },
+  },
 });

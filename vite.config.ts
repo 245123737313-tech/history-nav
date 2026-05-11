0bed1ed85610e2525f9b65291e42560d3f11c5a7
245123737313-tech <245123737313@mvsrec.edu.in>
2026-05-11 15:48:08 +0530

    claudee

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  tanstackStart: {
-    server: {
-      entry: "src/server.ts",
-      preset: "vercel",
-    },
-  },
   vite: {
     build: {
       chunkSizeWarningLimit: 1200,

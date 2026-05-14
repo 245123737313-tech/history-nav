222befe2c938f057f13a2a998162476c128b03dd
ChallaSravanReddy <sravanreddychalla609@gmail.com>
2026-05-14 15:58:57 +0530

    fixed everything

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  ssr: true,
   vite: {
     build: {
       chunkSizeWarningLimit: 1200,
     },
   },
 });
+
